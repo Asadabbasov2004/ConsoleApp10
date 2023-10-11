caa3a16d8f333b14f0501bab365324838c85f80f
II novbe <II novbe@DESKTOP-4EOUDP3>
2023-10-11 19:31:27 +0400

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("xos gelmisen zehmet olmasa daxil ol");
+            //User user = new User();
+            //Console.WriteLine("adinizi girin");
+            //user.Name = Console.ReadLine();
+            //Console.WriteLine("soyadiniz girin");
+            //user.Surname = Console.ReadLine();
+            //user.Register();
+            User user = new User();
+            string a= Console.ReadLine();
+            switch (a)
+            {
+
+                    default:
+ break;
+            }
+            user.Register();
+            user.Login();
+        }
+    }
+}
